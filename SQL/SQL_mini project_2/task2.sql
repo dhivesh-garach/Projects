cf25f5ffd02ec4e7f2e9c01d1af6f039c808c3d2
Dhivesh Garach <71030962+dhivesh-garach@users.noreply.github.com>
2023-05-04 05:24:17 +0530

    complex sql queries for analyzing supply chain data

--- SQL/SQL_mini project_2/task2.sql
+++ SQL/SQL_mini project_2/task2.sql
@@ -1,11 +1,14 @@
-/* Solution 1 */
+/* One of the reporting view that the business wants to setup is to track how the sales reps are performing. There is a need to track the following:
+
+1. Which sales reps are handling which accounts? Your output should look as given below: */
+
 Select a.sales_rep_id, s.name as "sales_rep_name", s.region_id as "sales_rep_region", a.name as "account_name", 
 Row_number() over (Partition By s.region_id, s.name) as "account_num"
 From sales_rep s INNER JOIN accounts a
 ON s.ID = a.sales_rep_id
 LIMIT 100;
 
-/* Solution 2 */
+/* 2. One of the aspects that the business wants to explore is what has been the share of each sales representative's s year on year sales out of the total yearly sales. */
 
 WITH stb AS (
 	Select extract(year from occured_at) as years, s.name as sales_rep_name, SUM(total_amount_usd) as total_sales
@@ -19,7 +22,8 @@ Select years, sales_rep_name, round((100*total_sales/SUM(total_sales) OVER (Part
 Rank() over (Partition By years Order by total_sales desc) as rank_sales_rep
 from stb;
 
-/* Solution 3*/
+/* Repeating the analysis given above but this time for region. Generated the percentage contribution of each region to total yearly revenue over years */
+
 With rtb AS (
 	Select extract(year from occured_at) as years, r.name as Region, sum(total_amount_usd) as Revenue
 	From region r INNER JOIN sales_rep s
--- SQL/SQL_mini project_2/task3.sql
+++ SQL/SQL_mini project_2/task3.sql
@@ -1,3 +1,7 @@
+/* Understanding which accounts contribute to the bulk of the revenue and the business also wants to see year on year trend on the revenue contribution of each account. */
+
+/* The final table should show revenue share of each account for each year's total revenue */
+
 With artb AS (
 	SELECT Extract(YEAR from occured_at) as years, a.name as acc_name, SUM(total_amount_usd) as revenue
 	FROM accounts a INNER JOIN orders o
